9f47fcf96e08b2994d5fcc0a17d3f75c3324b489
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2024-11-08 12:18:21 +0600

    initial commit

---
+++ agenda.php
@@ -0,0 +1,11 @@
+<?php
+/*
+
+1.Title: Introduction to MVC Architecture in PHP
+
+Agenda
+1.Understanding MVC: History and Evolution
+2.MVC Structure Overview: Model, View, and Controller Roles
+3.Benefits of MVC: Separation of Concerns, Maintainability, Scalability
+
+*/ 
